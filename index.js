a9bdde7dd32cd74b3a12c154c9b9dfb8a07dec57
richie <richardpeterprince@gmail.com>
2025-04-13 17:55:20 -0500

    newest updates

---
+++ index.js
@@ -0,0 +1,36 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+// Get a reference to the button
+const bgButton = document.querySelector('#bg-button');
+
+// Add an event listener to the button
+bgButton.addEventListener('click', function() {
+	// When the button is clicked, update the background color of the body on all pages
+	document.querySelectorAll('body').forEach(function(body) {
+	  body.style.backgroundColor = 'black';
+	});
+  });
